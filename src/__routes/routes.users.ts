7ca6f51bbbcc71e3c400b64585ba97b7e0d39510
davmaene <davidmened@gmail.com>
2024-03-15 17:39:59 +0200

    up --version done

--- src/__routes/routes.users.ts
+++ src/__routes/routes.users.ts
@@ -1,11 +1,11 @@
-import { onValidate, userModelOnSignin } from '../__middlewares/middleware.datavalidator'
+import { onValidate, userModelOnSignin, userModelValidator } from '../__middlewares/middleware.datavalidator'
 import { __controllerUsers } from '../__controllers/controller.users'
 import express from 'express'
 
 export const __routesUsers = express.Router()
 
 __routesUsers.post("/user/auth", onValidate(userModelOnSignin), __controllerUsers.auth)
-__routesUsers.post("/user/register", __controllerUsers.register)
+__routesUsers.post("/user/register", onValidate(userModelValidator),__controllerUsers.register)
 
 // __routesUsers.post("/user/add",  __controlerUsers.savenew) // onValidate(userModelValidator)
 // __routesUsers.post("/user/verify", onValidate(userModelOnVerification), limiterVerify, __controlerUsers.verify)
