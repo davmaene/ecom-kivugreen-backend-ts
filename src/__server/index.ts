afb527c37b62f31da7b07a468b5ebb6ded8fd807
davmaene <davidmened@gmail.com>
2024-03-16 18:14:27 +0200

    ip -- version done

--- src/__server/index.ts
+++ src/__server/index.ts
@@ -6,6 +6,7 @@ import { Responder } from "../__helpers/helper.responseserver";
 import * as fs from 'fs';
 import * as path from 'path';
 import morgan from 'morgan';
+import fileUpload from 'express-fileupload'
 import { accessValidator } from "../__middlewares/middleware.accessvalidator";
 import { routes } from "../__routes/index";
 
@@ -19,6 +20,10 @@ const PORT = APP_PORT || 8012;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use(fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+}));
 
 app.use((req: Request, res: Response, next: NextFunction) => {
     res.header("Access-Control-Allow-Origin", "*");
