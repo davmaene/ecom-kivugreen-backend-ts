ea6ca002022cd2e3217884eb9524ad1a7dd5cc92
davmaene <davidmened@gmail.com>
2024-03-18 21:12:06 +0200

    up --verion done

--- src/__routes/routes.cooperatives.ts
+++ src/__routes/routes.cooperatives.ts
@@ -1,5 +1,8 @@
 import { __controllerCooperatives } from '../__controllers/controller.cooperatives';
+import { coopecModelValidator, onValidate, userModelOnSignin, userModelValidator } from '../__middlewares/middleware.datavalidator'
+
 import express from 'express';
 
 export const __routesCooperatives = express.Router()
 __routesCooperatives.get("/list", __controllerCooperatives.list)
+__routesCooperatives.post("/cooperative/add", onValidate(coopecModelValidator), __controllerCooperatives.add)
