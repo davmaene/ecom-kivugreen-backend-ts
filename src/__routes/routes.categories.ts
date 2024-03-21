b5bde334ae3e8e203acc84dbcaa54dc9c42dd3ba
davmaene <davidmened@gmail.com>
2024-03-21 14:04:00 +0200

    up --version done

--- src/__routes/routes.categories.ts
+++ src/__routes/routes.categories.ts
@@ -2,5 +2,6 @@ import { __controllerCategories } from '../__controllers/controller.categories'
 import express from 'express'
 
 export const __routesCategories = express.Router()
+
 __routesCategories.get('/list', __controllerCategories.list)
 __routesCategories.post('/categorie/add', __controllerCategories.add)
