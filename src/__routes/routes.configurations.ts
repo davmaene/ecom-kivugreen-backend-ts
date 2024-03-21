d207a685de02ed15eb9b2f27d63a8b186fd6d3b9
davmaene <davidmened@gmail.com>
2024-03-21 16:05:15 +0200

    up --verion done

--- src/__routes/routes.configurations.ts
+++ src/__routes/routes.configurations.ts
@@ -2,5 +2,6 @@ import { __controllerConfigs } from '../__controllers/controller.configs'
 import express from 'express'
 
 export const __routesConfigurations = express.Router()
+
 __routesConfigurations.put("/configuration", __controllerConfigs.add)
 __routesConfigurations.get("/get", __controllerConfigs.get)
