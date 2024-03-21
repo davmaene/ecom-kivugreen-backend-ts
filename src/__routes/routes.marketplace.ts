2c8fcfc52d6216e047dfe3a98dfe80adeaaf9081
davmaene <davidmened@gmail.com>
2024-03-21 16:04:40 +0200

    up --version done

--- src/__routes/routes.marketplace.ts
+++ src/__routes/routes.marketplace.ts
@@ -2,6 +2,7 @@ import { __controllerMarketplace } from '../__controllers/controller.marketplace
 import express from 'express'
 
 export const __routesMarketplace = express.Router()
+
 __routesMarketplace.get('/', __controllerMarketplace.marketplace)
 __routesMarketplace.post('/command', __controllerMarketplace.placecommand)
 __routesMarketplace.post('/card/addtopanier', __controllerMarketplace.addtopanier)
