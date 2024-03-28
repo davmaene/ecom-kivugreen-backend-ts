0e1554abfe85fc4f5b62de2d19bbfe5b6375a514
davmaene <davidmened@gmail.com>
2024-03-29 00:09:38 +0200

    up --version done

--- src/__controllers/controller.commandes.ts
+++ src/__controllers/controller.commandes.ts
@@ -3,21 +3,25 @@ import { HttpStatusCode } from "../__enums/enum.httpsstatuscode";
 import { Responder } from "../__helpers/helper.responseserver";
 import { NextFunction, Request, Response } from "express";
 import { Produits } from "../__models/model.produits";
+import { Typelivraisons } from "../__models/model.typelivraison";
 
 export const __controllerCommandes = {
     list: async (req: Request, res: Response) => {
         try {
             Commandes.belongsTo(Produits, { foreignKey: "id_produit" })
+            Commandes.belongsTo(Typelivraisons, { foreignKey: "type_livraison" })
             Commandes.findAll({
                 include: [
                     {
                         model: Produits,
                         required: false,
+                    },
+                    {
+                        model: Typelivraisons,
+                        required: false,
                     }
                 ],
-                where: {
-
-                }
+                where: {}
             })
                 .then(commandes => {
                     return Responder(res, HttpStatusCode.Ok, { count: commandes.length, rows: commandes })
@@ -59,6 +63,32 @@ export const __controllerCommandes = {
     listbystate: async (req: Request, res: Response) => {
         const { status } = req.params;
         if (!status) return Responder(res, HttpStatusCode.NotAcceptable, "this request must have at least status in the request !")
+        try {
+            Commandes.belongsTo(Produits, { foreignKey: "id_produit" })
+            Commandes.findAll({
+                include: [
+                    {
+                        model: Produits,
+                        required: false,
+                    }
+                ],
+                where: {
+                    state: parseInt(status)
+                }
+            })
+                .then(commandes => {
+                    return Responder(res, HttpStatusCode.Ok, { count: commandes.length, rows: commandes })
+                })
+                .catch(err => {
+                    return Responder(res, HttpStatusCode.InternalServerError, err)
+                })
+        } catch (error) {
+            return Responder(res, HttpStatusCode.InternalServerError, error)
+        }
+    },
+    validate: async (req: Request, res: Response) => {
+        const { idcommande } = req.params;
+        if (!idcommande) return Responder(res, HttpStatusCode.NotAcceptable, "this request must have at least idcommande in the request !")
         try {
             Commandes.belongsTo(Produits, { foreignKey: "id_produit" })
             Commandes.findAll({
