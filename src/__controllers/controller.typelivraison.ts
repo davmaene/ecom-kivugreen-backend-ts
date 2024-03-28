9e1944289a35d4d18458ae0db769baab93ed6d8a
davmaene <davidmened@gmail.com>
2024-03-28 22:27:09 +0200

    up --done adding type livraison

--- src/__controllers/controller.typelivraison.ts
+++ src/__controllers/controller.typelivraison.ts
@@ -32,7 +32,7 @@ export const __controllerTypelivraison = {
             })
                 .then((typel) => {
                     if (typel instanceof Typelivraisons)
-                        return Responder(res, HttpStatusCode.Ok, type)
+                        return Responder(res, HttpStatusCode.Ok, typel)
                 })
                 .catch(err => {
                     return Responder(res, HttpStatusCode.Conflict, err)
@@ -41,5 +41,47 @@ export const __controllerTypelivraison = {
             return Responder(res, HttpStatusCode.InternalServerError, error)
         }
     },
-
+    update: async (req: Request, res: Response, next: NextFunction) => {
+        const { frais_livraison, quantite, type, description, lieux } = req.body
+        if (Object.keys(req.body).length <= 0) return Responder(res, HttpStatusCode.NotAcceptable, "This request must have at least !frais_livraison || !quantite || !type || !description || !lieux")
+        try {
+            Typelivraisons.create({
+                ...req.body
+                // frais_livraison: parseFloat(frais_livraison),
+                // quantite: parseInt(quantite),
+                // type,
+                // description,
+                // lieux
+            })
+                .then((typel) => {
+                    return Responder(res, HttpStatusCode.Ok, type)
+                })
+                .catch(err => {
+                    return Responder(res, HttpStatusCode.Conflict, err)
+                })
+        } catch (error) {
+            return Responder(res, HttpStatusCode.InternalServerError, error)
+        }
+    },
+    delete: async (req: Request, res: Response, next: NextFunction) => {
+        const { id } = req.params
+        if (!id) return Responder(res, HttpStatusCode.NotAcceptable, "This request must have at least !frais_livraison || !quantite || !type || !description || !lieux")
+        try {
+            Typelivraisons.destroy({
+                where: {
+                    id
+                }
+            })
+                .then((typel) => {
+                    if (typel !== 0)
+                        return Responder(res, HttpStatusCode.Ok, `Item successfuly deleted !`)
+                    else return Responder(res, HttpStatusCode.Conflict, `Item with status ${id} was not found !`)
+                })
+                .catch(err => {
+                    return Responder(res, HttpStatusCode.Conflict, err)
+                })
+        } catch (error) {
+            return Responder(res, HttpStatusCode.InternalServerError, error)
+        }
+    },
 }
--- src/__routes/routes.typelivraisons.ts
+++ src/__routes/routes.typelivraisons.ts
@@ -3,3 +3,6 @@ import express from 'express'
 
 export const __routesTypelivraison = express.Router()
 __routesTypelivraison.get("/list", __controllerTypelivraison.list)
+__routesTypelivraison.put("/typeslivraison/:id", __controllerTypelivraison.update)
+__routesTypelivraison.delete("/typeslivraison/:id", __controllerTypelivraison.delete)
+__routesTypelivraison.post("/typeslivraison/add", __controllerTypelivraison.add)
