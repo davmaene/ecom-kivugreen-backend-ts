996888afb9b1bdc1b6288ab600a58f20315371ae
davmaene <davidmened@gmail.com>
2024-03-29 14:32:07 +0200

    up --version done

--- src/__controllers/controller.members.ts
+++ src/__controllers/controller.members.ts
@@ -1,14 +1,27 @@
-import { HttpStatusCode } from "__enums/enum.httpsstatuscode";
-import { Responder } from "__helpers/helper.responseserver";
+import { HttpStatusCode } from "../__enums/enum.httpsstatuscode";
+import { Responder } from "../__helpers/helper.responseserver";
+import { Hasmembers } from "../__models/model.hasmembers";
 import { NextFunction, Request, Response } from "express";
 
 export const __controllerMembers = {
     list: async (req: Request, res: Response, next: NextFunction) => {
-
+        try {
+            Hasmembers.findAll({
+                where: {}
+            })
+                .then(list => {
+                    return Responder(res, HttpStatusCode.Ok, { count: list.length, rows: list })
+                })
+                .catch(err => {
+                    return Responder(res, HttpStatusCode.InternalServerError, err)
+                })
+        } catch (error) {
+            return Responder(res, HttpStatusCode.InternalServerError, error)
+        }
     },
     listbycooperative: async (req: Request, res: Response) => {
         const { idcooperative } = req.params;
-        if(idcooperative) return Responder(res, HttpStatusCode.NotAcceptable, "this request must have at least idcooperative ")
+        if (idcooperative) return Responder(res, HttpStatusCode.NotAcceptable, "this request must have at least idcooperative ")
         try {
 
         } catch (error) {
---
+++ src/__routes/routes.memebers.ts
@@ -0,0 +1,7 @@
+import { __controllerMembers } from '../__controllers/controller.members'
+import express from 'express'
+
+export const __routesMembers = express.Router()
+
+__routesMembers.get("/list", __controllerMembers.list)
+__routesMembers.get("/list/bycooperative/:idcooperative", __controllerMembers.listbycooperative)
