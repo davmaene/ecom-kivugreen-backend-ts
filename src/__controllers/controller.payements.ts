1e62491374508bf73f0bd3a98ecd5f7d1ea7287c
davmaene <davidmened@gmail.com>
2024-04-07 13:45:11 +0200

    up me line

---
+++ src/__controllers/controller.payements.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+
+export const __controllerPayements = {
+    list: async (req: Request, res: Response, next: NextFunction) => {
+
+    },
+    listbyowner: async (req: Request, res: Response, next: NextFunction) => {
+
+    }
+}
---
+++ src/__routes/routes.payements.ts
@@ -0,0 +1,6 @@
+import { __controllerPayements } from '__controllers/controller.payements'
+import express from 'express'
+
+export const __routesPayements = express.Router()
+__routesPayements.get("/list", __controllerPayements.list)
+__routesPayements.get("/list/byowner", __controllerPayements.listbyowner)
