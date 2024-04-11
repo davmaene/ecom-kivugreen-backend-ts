dcafe388f42993e599dc2928a0caea40dcf1a29e
davmaene <davidmened@gmail.com>
2024-04-11 12:46:57 +0200

    up me line

--- src/__routes/routes.produits.ts
+++ src/__routes/routes.produits.ts
@@ -5,3 +5,5 @@ import express from 'express'
 export const __routesProduits = express.Router()
 __routesProduits.post('/produit/add', onValidate(produitValidator), __controllerProduits.add)
 __routesProduits.get('/list', __controllerProduits.list)
+__routesProduits.put('/produit/:idproduit', __controllerProduits.update)
+__routesProduits.delete('/produit/:idproduit', __controllerProduits.delete)
