8f889a66b642296be6f5e97e0274d31a1aa9c66d
davmaene <davidmened@gmail.com>
2024-06-10 10:22:21 +0200

    up me line

--- src/__services/services.payements.ts
+++ src/__services/services.payements.ts
@@ -178,34 +178,33 @@ export const Payements = {
                     // console.log("Message from transacrion is ==> ", message, transaction, "The value of the state is ===> ", status);
                     if (asstatus === '0' || asstatus === 0) {
                         if (1) {
-                            Commandes.update({
-                                state: 3
-                            }, {
-                                where: {
-                                    transaction: idtransaction
-                                }
+                            const cmds = await Commandes.findAll({ where: { transaction: idtransaction } })
+                            for (let index = 0; index < cmds.length; index++) {
+                                const cmd = cmds[index];
+                                cmd.update({
+                                    state: 3
+                                })
+                                    .then(__ => { })
+                                    .catch(err => {
+                                        Services.onSendSMS({
+                                            is_flash: false,
+                                            to: fillphone({ phone: customer_phone || phone }),
+                                            content: `Désolé votre paiement de ${amount}${currency} est en cours de traietement ID:${realref}`
+                                        })
+                                            .then(_ => { })
+                                            .catch(_ => { })
+                                        return reject({ code: 500, message: "An error occured when trying to resolve payement !", data: data })
+                                    })
+                            }
+                            p.update({ status: 2 })
+                            Services.onSendSMS({
+                                is_flash: false,
+                                to: fillphone({ phone: customer_phone || phone }),
+                                content: `Félicitations votre paiement de ${amount}${currency} a été reçu avec succès ID:${realref}`
                             })
-                                .then(__ => {
-                                    p.update({ status: 2 })
-                                    Services.onSendSMS({
-                                        is_flash: false,
-                                        to: fillphone({ phone: customer_phone || phone }),
-                                        content: `Félicitations votre paiement de ${amount}${currency} a été reçu avec succès !ID:${idtransaction}`
-                                    })
-                                        .then(_ => { })
-                                        .catch(_ => { })
-                                    return resolve({ code: 200, message: "Transaction done resolved !", data: data })
-                                })
-                                .catch(err => {
-                                    Services.onSendSMS({
-                                        is_flash: false,
-                                        to: fillphone({ phone: customer_phone || phone }),
-                                        content: `Désolé votre paiement de ${amount}${currency} est en cours de traietement !ID:${idtransaction}`
-                                    })
-                                        .then(_ => { })
-                                        .catch(_ => { })
-                                    return reject({ code: 500, message: "An error occured when trying to resolve payement !", data: data })
-                                })
+                                .then(_ => { })
+                                .catch(_ => { })
+                            return resolve({ code: 200, message: "Transaction done resolved !", data: data })
                         } else {
                             Services.onSendSMS({
                                 is_flash: false,
