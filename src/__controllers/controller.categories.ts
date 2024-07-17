af2e8f3160ec87ee8a61043b9104f51f0cf583af
davmaene <davidmened@gmail.com>
2024-07-17 13:20:30 +0200

    up me done delete category and subcategory

--- src/__controllers/controller.categories.ts
+++ src/__controllers/controller.categories.ts
@@ -17,16 +17,66 @@ export const __controllerCategories = {
     },
     add: async (req: Request, res: Response) => {
         const { category, description } = req.body
-        if(!category || !description) return Responder(res, HttpStatusCode.NotAcceptable, "This request must have at least !category || !description")
+        if (!category || !description) return Responder(res, HttpStatusCode.NotAcceptable, "This request must have at least !category || !description")
         try {
             Categories.create({
                 category,
                 description
             })
-            .then(cat => {
-                if(cat instanceof Categories) return Responder(res, HttpStatusCode.Created, cat)
-                else return Responder(res, HttpStatusCode.BadRequest, cat)
+                .then(cat => {
+                    if (cat instanceof Categories) return Responder(res, HttpStatusCode.Created, cat)
+                    else return Responder(res, HttpStatusCode.BadRequest, cat)
+                })
+        } catch (error) {
+            return Responder(res, HttpStatusCode.InternalServerError, error)
+        }
+    },
+    update: async (req: Request, res: Response) => {
+        const { id } = req.params as any
+        const { category, description } = req.body;
+        if (!id) return Responder(res, HttpStatusCode.NotAcceptable, "This request must have at least id")
+        if (Object.keys(req.body).length <= 0) return Responder(res, HttpStatusCode.NotAcceptable, "the body should not be empty!")
+        try {
+            Categories.findOne({
+                where: {
+                    id
+                }
             })
+                .then(cat => {
+                    if (cat instanceof Categories) {
+                        cat.update({
+                            category,
+                            description
+                        })
+                            .then(_ => Responder(res, HttpStatusCode.Ok, cat))
+                            .catch(__ => Responder(res, HttpStatusCode.NotFound, "Item not found"))
+                    } else {
+                        return Responder(res, HttpStatusCode.NotFound, "Item not found")
+                    }
+                })
+                .catch(Err => Responder(res, HttpStatusCode.NotAcceptable, Err))
+        } catch (error) {
+            return Responder(res, HttpStatusCode.InternalServerError, error)
+        }
+    },
+    delete: async (req: Request, res: Response,) => {
+        const { id } = req.params as any
+        if (!id) return Responder(res, HttpStatusCode.NotAcceptable, "This request must have at least id")
+        try {
+            Categories.findOne({
+                where: {
+                    id
+                }
+            })
+                .then(cat => {
+                    if (cat instanceof Categories) {
+                        cat.destroy()
+                            .then(_ => Responder(res, HttpStatusCode.Ok, `Item with id:::${id} was successfuly deleted `))
+                    } else {
+                        return Responder(res, HttpStatusCode.NotFound, "Item not found")
+                    }
+                })
+                .catch(Err => Responder(res, HttpStatusCode.NotAcceptable, Err))
         } catch (error) {
             return Responder(res, HttpStatusCode.InternalServerError, error)
         }
--- src/__routes/routes.souscategories.ts
+++ src/__routes/routes.souscategories.ts
@@ -2,5 +2,8 @@ import { __controllerSouscategories } from '../__controllers/controller.souscate
 import express from 'express'
 
 export const __routesSouscategories = express.Router()
+
 __routesSouscategories.get('/list', __controllerSouscategories.list)
 __routesSouscategories.post('/souscategorie/add', __controllerSouscategories.add)
+__routesSouscategories.put('/souscategorie/:id', __controllerSouscategories.update)
+__routesSouscategories.delete('/souscategorie/:id', __controllerSouscategories.delete)
